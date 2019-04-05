a341cb9b5f4455bc6653ec4d185f59f45466c036
Hyeon Jun Kim <hjk013@gmail.com>
2019-04-04 23:42:35 -0700

    prettier/eslint config

--- rest_server/server.js
+++ rest_server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const path = require("path");
 const parser = require("body-parser");
+const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
+
+//Gmail API
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLEINT_SECRET;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
 const app = express();
 const PORT = process.env.PORT || 1231;
@@ -16,3 +23,24 @@ app.get(" * ", (req, res) => {
 // app.use('/api', router);
 
 app.listen(PORT, () => console.log(`Successfully connected to PORT: ${PORT}`));
+
+
+//nodemailer 
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    xoauth2: xoauth2.createXOAuth2Generator({
+      user: 'hjk013@gmail.com',
+      clinetId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESH_TOKEN,
+    })
+  }
+})
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
