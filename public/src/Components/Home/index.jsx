20523d640a9957203a063b78518825be54bf32a5
Hyeon Jun Kim <hjk013@gmail.com>
2019-04-22 23:33:04 -0700

    changed mainpage style + added link buttons

--- public/src/Components/Home/index.jsx
+++ public/src/Components/Home/index.jsx
@@ -5,9 +5,20 @@ const Home = () => (
   <div id="home">
     <div className="home-container">
       <div className="text-box">
-        <h1 className="home-h1">Hello, I'm Jun</h1>
-        <h2 className="home-h2">I'm a Software Engineer</h2>
+        <h1 className="home-h1">Jun Kim</h1>
+        <h2 className="home-h2">Software Engineer</h2>
       </div>
+    </div>
+    <div className="social-btns">
+      <a className="btn facebook" href="#">
+        <i className="fab fa-linkedin-in" />
+      </a>
+      <a className="btn github" href="#">
+        <i className="fab fa-github" />
+      </a>
+      <a className="btn resume" href="#">
+        <i className="fab fa-adobe" />
+      </a>
     </div>
   </div>
 );
