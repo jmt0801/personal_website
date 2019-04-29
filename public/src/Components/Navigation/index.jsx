6a2216018285a96313713e663d32ff420f755409
Hyeon Jun Kim <hjk013@gmail.com>
2019-04-29 02:53:22 -0700

    regular navbar

--- public/src/Components/Navigation/index.jsx
+++ public/src/Components/Navigation/index.jsx
@@ -15,6 +15,18 @@ class Navigation extends React.Component {
         <TabletBreakpoint>
           <Hamburger />
         </TabletBreakpoint>
+        <LaptopBreakpoint>
+          <div className="nav-bar-container">
+            <div className="nav-bar">
+              <ul className="nav-menu">
+                <li className="nav-menu-items">About Me</li>
+                <li className="nav-menu-items">Skills</li>
+                <li className="nav-menu-items">Recent Work</li>
+                <li className="nav-menu-items">Contact</li>
+              </ul>
+            </div>
+          </div>
+        </LaptopBreakpoint>
       </div>
     );
   }
