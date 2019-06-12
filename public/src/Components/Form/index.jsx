7d6dc9047410701ca02b6d231bfef1b6580bb599
Hyeon Jun Kim <hjk013@gmail.com>
2019-06-12 16:00:38 -0700

    data-netlify true added

--- public/src/Components/Form/index.jsx
+++ public/src/Components/Form/index.jsx
@@ -32,6 +32,8 @@ class Form extends Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": "contact",
+        "data-netlify": "true",
+        "data-netlify-honeypot": "bot-field",
         ...this.state
       })
     })
